cfdc78469cd9fabe7b6ca41f73206019fa156644
setxand <setxand@gmail.com>
2019-04-17 10:42:05 +0300

    Refactoring

--- src/main/java/com/example/demo/controller/telegramControllers/telegramWebHookController.java
+++ src/main/java/com/example/demo/controller/telegramControllers/telegramWebHookController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller.telegramControllers;
 
-import com.example.demo.model.telegram.Update;
 import com.example.demo.services.telegramService.UpdateParserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import telegram.Update;
 
 @RestController
 @RequestMapping("/telegramWebHook")
